29cd8bb7863acfd24027728140d1d7fc6d7cd676
fujipooo <majesty1579@yahoo.co.jp>
2019-07-28 01:18:46 +0900

    メッセージ送信機能完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_group
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
-    message = Message.create(content: message_params[:text],image: message_params[:image] ,user_id: current_user.id, group_id: message_params[:group_id])
-    redirect_to "/groups/#{message.group.id}" 
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @message = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
   end
 
   private
 
   def message_params
-    params.permit(:text :group_id :image)
+    params.require(:message).permit(:content,:image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
