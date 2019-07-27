0f3003402f388c54b46ae6fe2c8e99c5b696ad75
fujipooo <majesty1579@yahoo.co.jp>
2019-07-27 18:44:07 +0900

    データ修復

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
   def index
+    
   end
 
   def new
@@ -9,7 +10,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+    set_group
   end
 
   def create
