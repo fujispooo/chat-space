9ac02e8ec1297246702990470947f3865e676e27
fujipooo <majesty1579@yahoo.co.jp>
2019-07-27 19:22:23 +0900

    Carrierwaveを導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,13 @@ class MessagesController < ApplicationController
   end
 
   def create
+    message = Message.create(content: message_params[:text],image: message_params[:image] ,user_id: current_user.id, group_id: message_params[:group_id])
+    redirect_to "/groups/#{message.group.id}" 
+  end
+
+  private
+
+  def message_params
+    params.permit(:text :group_id :image)
   end
 end
